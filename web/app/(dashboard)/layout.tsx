bca607526a41fe41c1ca07ce47f5a8c91e8e520c
muriukialex <alexmuriukimaina254@gmail.com>
2025-01-31 16:57:03 +0300

    - Add Suspense boundary

--- web/app/(dashboard)/layout.tsx
+++ web/app/(dashboard)/layout.tsx
@@ -2,9 +2,14 @@
 
 import { AuthProvider } from '@/context/auth-context';
 import type { FC, PropsWithChildren } from 'react';
+import { Suspense } from 'react';
 
 const DashboardLayout: FC<PropsWithChildren> = function ({ children }) {
-    return <AuthProvider>{children}</AuthProvider>;
+    return (
+        <AuthProvider>
+            <Suspense>{children}</Suspense>
+        </AuthProvider>
+    );
 };
 
 export default DashboardLayout;
