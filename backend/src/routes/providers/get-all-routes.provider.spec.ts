617dbdb60f401eab68d340b3b41783a78dbd3054
muriukialex <alexmuriukimaina254@gmail.com>
2025-02-16 21:03:46 +0300

    - Add even more tests

--- backend/src/routes/providers/get-all-routes.provider.spec.ts
+++ backend/src/routes/providers/get-all-routes.provider.spec.ts
@@ -1,18 +1,99 @@
+import { RequestTimeoutException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { PaginationQueryDto } from 'src/common/pagination/dtos/pagination-query-dto';
+import { PaginationInterface } from 'src/common/pagination/interfaces/pagination.interface';
+import { PaginationProvider } from 'src/common/pagination/providers/pagination.provider';
+import { Repository } from 'typeorm';
+import { Route } from '../route.entity';
 import { GetAllRoutesProvider } from './get-all-routes.provider';
 
 describe('GetAllRoutesProvider', () => {
   let provider: GetAllRoutesProvider;
+  let paginationProvider: PaginationProvider;
+  let routeRepository: jest.Mocked<Repository<Route>>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [GetAllRoutesProvider],
+      providers: [
+        GetAllRoutesProvider,
+        {
+          provide: PaginationProvider,
+          useValue: {
+            paginateQuery: jest.fn(),
+          },
+        },
+        {
+          provide: getRepositoryToken(Route),
+          useValue: {},
+        },
+      ],
     }).compile();
 
     provider = module.get<GetAllRoutesProvider>(GetAllRoutesProvider);
+    paginationProvider = module.get<PaginationProvider>(PaginationProvider);
+    routeRepository = module.get(getRepositoryToken(Route));
   });
 
   it('should be defined', () => {
     expect(provider).toBeDefined();
+  });
+
+  it('should get all routes successfully', async () => {
+    const mockRoute: Route = {
+      createdAt: new Date(),
+      destination: 'Bungoma',
+      distance: 340,
+      id: 10,
+      origin: 'Nairobi',
+      travellingBuses: [],
+    };
+
+    const mockUserBookings: PaginationInterface<Route> = {
+      data: [mockRoute],
+      meta: {
+        currentPage: 1,
+        itemsPerPage: 5,
+        totalItems: 1,
+        totalPages: 1,
+      },
+      links: {
+        first: '',
+        last: '',
+        current: '',
+        next: '',
+        previous: '',
+      },
+    };
+    (paginationProvider.paginateQuery as jest.Mock).mockResolvedValue(
+      mockUserBookings,
+    );
+
+    const mockPaginationQuery: PaginationQueryDto = {
+      page: 1,
+      per: 10,
+    };
+    const result = await provider.getAllRoutes(mockPaginationQuery);
+
+    expect(paginationProvider.paginateQuery).toHaveBeenCalledWith(
+      mockPaginationQuery,
+      routeRepository,
+    );
+    expect(result).toEqual(mockUserBookings);
+  });
+
+  it('should throw RequestTimeoutException if failed to get routes', async () => {
+    (paginationProvider.paginateQuery as jest.Mock).mockRejectedValue(
+      new Error('Error fetching routes'),
+    );
+
+    const mockPaginationQuery: PaginationQueryDto = {
+      page: 1,
+      per: 10,
+    };
+
+    expect(provider.getAllRoutes(mockPaginationQuery)).rejects.toThrow(
+      RequestTimeoutException,
+    );
   });
 });
--- backend/src/routes/providers/routes.service.spec.ts
+++ backend/src/routes/providers/routes.service.spec.ts
@@ -1,12 +1,88 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { PaginationInterface } from 'src/common/pagination/interfaces/pagination.interface';
+import { CreateNewRouteDto } from '../dtos/create-new-route.dto';
+import { Route } from '../route.entity';
+import { CreateNewRouteProvider } from './create-new-route.provider';
+import { GetAllRoutesProvider } from './get-all-routes.provider';
+import { GetRouteByIdProvider } from './get-route-by-id.provider';
 import { RoutesService } from './routes.service';
 
 describe('RoutesService', () => {
+  const MockCreateNewRouteProvider = {
+    createNewRoute: (createNewRouteDto: CreateNewRouteDto) => {
+      return Promise.resolve({
+        id: 1,
+        origin: createNewRouteDto.origin,
+        destination: createNewRouteDto.destination,
+        distance: createNewRouteDto.distance,
+      });
+    },
+  };
+
+  const mockRoute: Route = {
+    createdAt: new Date(),
+    destination: 'Bungoma',
+    distance: 340,
+    id: 10,
+    origin: 'Nairobi',
+    travellingBuses: [],
+  };
+
+  const mockRoutes: PaginationInterface<Route> = {
+    data: [mockRoute],
+    meta: {
+      currentPage: 1,
+      itemsPerPage: 5,
+      totalItems: 1,
+      totalPages: 1,
+    },
+    links: {
+      first: '',
+      last: '',
+      current: '',
+      next: '',
+      previous: '',
+    },
+  };
+
+  const MockGetAllRoutesProvider = {
+    getAllRoutes: () => {
+      return Promise.resolve<PaginationInterface<Route>>(mockRoutes);
+    },
+  };
+
+  const MockGetRouteByIdProvider = {
+    getRouteById: () => {
+      return Promise.resolve<Route>({
+        createdAt: new Date(),
+        destination: 'Bungoma',
+        distance: 340,
+        id: 10,
+        origin: 'Nairobi',
+        travellingBuses: [],
+      });
+    },
+  };
+
   let service: RoutesService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [RoutesService],
+      providers: [
+        RoutesService,
+        {
+          provide: CreateNewRouteProvider,
+          useValue: MockCreateNewRouteProvider,
+        },
+        {
+          provide: GetAllRoutesProvider,
+          useValue: MockGetAllRoutesProvider,
+        },
+        {
+          provide: GetRouteByIdProvider,
+          useValue: MockGetRouteByIdProvider,
+        },
+      ],
     }).compile();
 
     service = module.get<RoutesService>(RoutesService);
@@ -14,5 +90,62 @@ describe('RoutesService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('createNewRoute', () => {
+    it('should test createNewRoute method on CreateNewRouteProvider', async () => {
+      const createBookingDto: CreateNewRouteDto = {
+        destination: 'Nairobi',
+        distance: 340,
+        origin: 'Bungoma',
+      };
+
+      const route: CreateNewRouteDto =
+        await service.createNewRoute(createBookingDto);
+      expect(route).toEqual(expect.objectContaining(route));
+    });
+  });
+
+  describe('getAllRoutes', () => {
+    it('should test getAllRoutes method on GetAllRoutesProvider', async () => {
+      const mockRoutes: PaginationInterface<Route> = {
+        data: [mockRoute],
+        meta: {
+          currentPage: 1,
+          itemsPerPage: 5,
+          totalItems: 1,
+          totalPages: 1,
+        },
+        links: {
+          first: '',
+          last: '',
+          current: '',
+          next: '',
+          previous: '',
+        },
+      };
+      const result = await service.getAllRoutes({
+        page: 1,
+        per: 10,
+      });
+
+      expect(result).toEqual(expect.objectContaining(mockRoutes));
+    });
+  });
+
+  describe('getRouteById', () => {
+    it('should test getRouteById method on GetRouteByIdProvider', async () => {
+      const mockRoute: Route = {
+        createdAt: new Date(),
+        destination: 'Bungoma',
+        distance: 340,
+        id: 10,
+        origin: 'Nairobi',
+        travellingBuses: [],
+      };
+
+      const result = await service.getRouteById(10);
+      expect(result).toEqual(mockRoute);
+    });
   });
 });
